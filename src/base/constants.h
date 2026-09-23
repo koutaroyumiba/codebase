643a7764d00c150d7e2c732ec7ad3b62048731bc
Kot <89110272+koutaroyumiba@users.noreply.github.com>
2026-09-24 00:57:15 +1200

    refactor(base): use base types

--- src/base/constants.h
+++ src/base/constants.h
@@ -1,23 +1,25 @@
 #ifndef CB_BASE_CONSTANTS_H
 #define CB_BASE_CONSTANTS_H
 
+#include <limits.h>
 #include <stdint.h>
 
-#define U8_MAX UINT8_MAX
-#define U16_MAX UINT16_MAX
-#define U32_MAX UINT32_MAX
-#define U64_MAX UINT64_MAX
+#define U8_MAX UCHAR_MAX
+#define U16_MAX USHRT_MAX
+#define U32_MAX UINT_MAX
+#define U64_MAX ULLONG_MAX
 #define USIZE_MAX SIZE_MAX
 
-#define I8_MIN INT8_MIN
-#define I8_MAX INT8_MAX
-#define I16_MIN INT16_MIN
-#define I16_MAX INT16_MAX
-#define I32_MIN INT32_MIN
-#define I32_MAX INT32_MAX
-#define I64_MIN INT64_MIN
-#define I64_MAX INT64_MAX
+#define I8_MIN SCHAR_MIN
+#define I16_MIN SHRT_MIN
+#define I32_MIN INT_MIN
+#define I64_MIN LLONG_MIN
 #define ISIZE_MIN PTRDIFF_MIN
+
+#define I8_MAX SCHAR_MAX
+#define I16_MAX SHRT_MAX
+#define I32_MAX INT_MAX
+#define I64_MAX LLONG_MAX
 #define ISIZE_MAX PTRDIFF_MAX
 
 #endif // CB_BASE_CONSTANTS_H
--- src/base/types.h
+++ src/base/types.h
@@ -3,26 +3,34 @@
 
 #include <limits.h>
 #include <stddef.h>
-#include <stdint.h>
 
-typedef uint8_t u8;
-typedef uint16_t u16;
-typedef uint32_t u32;
-typedef uint64_t u64;
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned int u32;
+typedef unsigned long long u64;
 typedef size_t usize;
 
-typedef int8_t i8;
-typedef int16_t i16;
-typedef int32_t i32;
-typedef int64_t i64;
+typedef char i8;
+typedef short i16;
+typedef int i32;
+typedef long long i64;
 typedef ptrdiff_t isize;
 
 typedef float f32;
 typedef double f64;
 
+// some checks to make sure:
 static_assert(CHAR_BIT == 8, "codebase requires 8-bit bytes");
-static_assert(sizeof(float) * CHAR_BIT == 32, "f32 requires a 32-bit float");
-static_assert(sizeof(double) * CHAR_BIT == 64, "f64 requires a 64-bit float");
+static_assert(sizeof(u8) * CHAR_BIT == 8, "u8 must occupy 8 bits");
+static_assert(sizeof(u16) * CHAR_BIT == 16, "u16 must occupy 16 bits");
+static_assert(sizeof(u32) * CHAR_BIT == 32, "u32 must occupy 32 bits");
+static_assert(sizeof(u64) * CHAR_BIT == 64, "u64 must occupy 64 bits");
+static_assert(sizeof(i8) * CHAR_BIT == 8, "i8 must occupy 8 bits");
+static_assert(sizeof(i16) * CHAR_BIT == 16, "i16 must occupy 16 bits");
+static_assert(sizeof(i32) * CHAR_BIT == 32, "i32 must occupy 32 bits");
+static_assert(sizeof(i64) * CHAR_BIT == 64, "i64 must occupy 64 bits");
+static_assert(sizeof(f32) * CHAR_BIT == 32, "f32 must occupy 32 bits");
+static_assert(sizeof(f64) * CHAR_BIT == 64, "f64 must occupy 64 bits");
 
 typedef struct {
   bool has_value;
